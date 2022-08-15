cacdc565c2ef02b4abd19e57ac76cd9cf3e873ae
Silv3S <slawomir.s.siwek@gmail.com>
2022-08-15 14:57:36 +0200

    Attention gates

--- architectures/DAREUNet.py
+++ architectures/DAREUNet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 from torch import nn
+from torchsummary import summary
 
 
 class DropBlock(nn.Module):
@@ -88,6 +89,30 @@ class UpsampleConvolve(nn.Module):
         return x
 
 
+class AttentionGate(nn.Module):
+    def __init__(self, g_channels, x_channels):
+        super(AttentionGate, self).__init__()
+        self.features_g = nn.Sequential(
+            nn.Conv2d(g_channels, x_channels, kernel_size=1),
+            nn.BatchNorm2d(x_channels))
+        self.features_x = nn.Sequential(
+            nn.Conv2d(x_channels, x_channels, kernel_size=1, stride=2),
+            nn.BatchNorm2d(x_channels))
+        self.psi = nn.Sequential(
+            nn.ReLU(inplace=True),
+            nn.Conv2d(x_channels, 1, kernel_size=1),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid(),
+            nn.Upsample(
+                scale_factor=2, mode='bilinear', align_corners=True))
+
+    def forward(self, g, x):
+        g_feat = self.features_g(g)
+        x_feat = self.features_x(x)
+        psi = self.psi(g_feat + x_feat)
+        return x * psi
+
+
 class SpatialAttention(nn.Module):
     def __init__(self):
         super(SpatialAttention, self).__init__()
@@ -117,12 +142,19 @@ class DARE_UNet(nn.Module):
         self.conv6 = SingleConv(512, 512)
 
         self.up1 = UpsampleConvolve(512, 256)
+        self.Att1 = AttentionGate(512, 256)
         self.uconv1 = DoubleConv(512, 256)
+
         self.up2 = UpsampleConvolve(256, 128)
+        self.Att2 = AttentionGate(256, 128)
         self.uconv2 = DoubleConv(256, 128)
+
         self.up3 = UpsampleConvolve(128, 64)
+        self.Att3 = AttentionGate(128, 64)
         self.uconv3 = DoubleConv(128, 64)
+
         self.up4 = UpsampleConvolve(64, 32)
+        self.Att4 = AttentionGate(64, 32)
         self.uconv4 = DoubleConv(64, 32)
 
         self.Final = nn.Sequential(
@@ -151,26 +183,28 @@ class DARE_UNet(nn.Module):
 
         # Decoder
         dec_1_ups = self.up1(bot_3)
-        dec_1_cat = torch.cat((enc_4, dec_1_ups), dim=1)
+        dec_1_att = self.Att1(bot_3, enc_4)
+        dec_1_cat = torch.cat((dec_1_att, dec_1_ups), dim=1)
         dec_1 = self.uconv1(dec_1_cat)
 
         dec_2_ups = self.up2(dec_1)
-        dec_2_cat = torch.cat((enc_3, dec_2_ups), dim=1)
+        dec_2_att = self.Att2(dec_1, enc_3)
+        dec_2_cat = torch.cat((dec_2_att, dec_2_ups), dim=1)
         dec_2 = self.uconv2(dec_2_cat)
 
         dec_3_ups = self.up3(dec_2)
-        dec_3_cat = torch.cat((enc_2, dec_3_ups), dim=1)
+        dec_3_att = self.Att3(dec_2, enc_2)
+        dec_3_cat = torch.cat((dec_3_att, dec_3_ups), dim=1)
         dec_3 = self.uconv3(dec_3_cat)
 
         dec_4_ups = self.up4(dec_3)
-        dec_4_cat = torch.cat((enc_1, dec_4_ups), dim=1)
+        dec_4_att = self.Att4(dec_3, enc_1)
+        dec_4_cat = torch.cat((dec_4_att, dec_4_ups), dim=1)
         dec_4 = self.uconv4(dec_4_cat)
 
         return self.Final(dec_4)
 
 
 if __name__ == "__main__":
-    x = torch.randn((4, 3, 256, 256))
     model = DARE_UNet()
-    preds = model(x)
-    assert preds.shape == (4, 1, 256, 256)
+    summary(model, (3, 256, 256), device='cpu')
